e03424d9a55069046f7508244fb5da6c1da36771
MrKaydee <44779363+koredebishi@users.noreply.github.com>
2022-07-08 06:21:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+ student_scores[n]=int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 
+#Write your code below this row 👇
+highest_score = 0
+for score in student_scores:
+  if score > highest_score :
+    highest_score = score
+print (highest_score)
+
+add_total = 0
+for scores in student_scores: 
+  add_total +=scores
+print (add_total)
+
+lenght = 0
+for n in student_scores:
+  if 
+
+    
+
+
+
+
